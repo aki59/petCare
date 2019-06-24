617e358531c0775a9eef2206acb01be0a1ce6852
akash tiwari <akash59tiwari@gmail.com>
2019-06-24 20:01:45 +0530

    Initial Commi

---
+++ src/main/java/com/PetOcare/PetOcareApplication.java
@@ -0,0 +1,13 @@
+package com.PetOcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetOcareApplication.class, args);
+	}
+
+}
