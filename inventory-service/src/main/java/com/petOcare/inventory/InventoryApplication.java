002c9989d7bd7f4fa99c7adaa37a8b70c75d9bbb
akash tiwari <akash59tiwari@gmail.com>
2019-06-26 19:36:25 +0530

    Changed Name from Adoption to Inventory

--- inventory-service/src/main/java/com/petOcare/inventory/InventoryApplication.java
+++ inventory-service/src/main/java/com/petOcare/inventory/InventoryApplication.java
@@ -1,13 +1,13 @@
-package com.petOcare.adoption;
+package com.petOcare.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdoptionApplication {
+public class InventoryApplication {
 	
 	public static void main(String... args) {
-		SpringApplication.run(AdoptionApplication.class, args);
+		SpringApplication.run(InventoryApplication.class, args);
 		
 	}
 
