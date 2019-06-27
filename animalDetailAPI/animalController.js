2f3829a8c56fdeab1798373b783457f6545c1a7b
akash tiwari <akash59tiwari@gmail.com>
2019-06-27 20:16:21 +0530

    Added Indian Dog Breeds

--- animalDetailAPI/animalController.js
+++ animalDetailAPI/animalController.js
@@ -1,8 +1,16 @@
 const express= require('express');
 const app=express();
+const filePath= '/Users/dell i5/git/petCare1/animalDetailAPI/Indian Pariah/Street-Dog-Madras-Courier-04.jpg';
+const dogBreeds= require('./IndianDogBreeds.json');
 
-app.get('/dogBreed',(req,res)=>{
-  res.send("husky");
+//app.use('/Indian Pariah', express.static(__dirname + '/Indian Pariah'));
+
+app.get('/api/IndianDogBreeds',(req,res)=>{
+  res.send(dogBreeds);
+});
+
+app.get('/api/:dogName',(req,res)=>{
+ res.sendFile(filePath);
 });
 
 app.listen(3000,()=> console.log('listening on port 3000'));
