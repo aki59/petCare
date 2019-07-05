e6818a6a96bbce3d925c63d969044ded610f2d2e
akash tiwari <akash59tiwari@gmail.com>
2019-07-05 22:04:02 +0530

    Added Role Table class

---
+++ auth-service/src/main/java/com/petOcare/security/dto/Role.java
@@ -0,0 +1,27 @@
+package com.petOcare.security.dto;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToMany(mappedBy="roles")
+	private Collection<User> users;
+	
+	private String roleName;
+	
+
+}
