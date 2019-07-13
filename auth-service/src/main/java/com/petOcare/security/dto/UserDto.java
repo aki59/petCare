0e3e92c07d0009b193367b0eaae2755795af8c3b
akash tiwari <akash59tiwari@gmail.com>
2019-07-13 16:59:53 +0530

    Initial Commit

---
+++ auth-service/src/main/java/com/petOcare/security/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.petOcare.security.dto;
+
+import com.petOcare.security.validation.ValidEmail;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserDto {
+    @NotNull
+    @Size(min = 1,message ="Please enter your FirstName")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1,message ="Please enter your LastName")
+    private String lastName;
+
+    private String password;
+
+    @ValidEmail
+    @NotNull
+    @Size(min = 1, message = "Enter valid emailID")
+    private String email;
+
+    private String roles;
+    private Integer age;
+}
